7044984ebf20e5182709b0664efd523a89b9e44f
Малиновський Константин <kostya.malinovskiy@gmail.com>
2018-01-04 23:36:09 +0200

    Changed interface. Comparator receives keys on initalize and objects to diff on diff

--- lib/object_diff/comparator.rb
+++ lib/object_diff/comparator.rb
@@ -1,33 +1,31 @@
 module ObjectDiff
   class Comparator
-    def initialize(a, b)
-      @a = a
-      @b = b
+    attr_reader :plain_key, :complex_key
+
+    def initialize(plain_key:[], complex_key:{})
+      @plain_key = plain_key
+      @complex_key = complex_key
     end
 
-    def diff_by(plain_key:[], complex_key:{})
+    def diff(a, b)
       Hash.new.tap do |diff|
         plain_key.each do |attr_name|
-          diff_attr = self.attr_diff(attr_name)
+          diff_attr = self.attr_diff(a, b, attr_name)
           diff[attr_name] = diff_attr if diff_attr
         end
 
         complex_key.each_pair do |attr_name, key|
-          diff[attr_name] = self.class.new(@a.send(attr_name), @b.send(attr_name)).diff_by(plain_key: key)
+          diff[attr_name] = self.class.new(plain_key: key).diff(a.send(attr_name), b.send(attr_name))
         end
       end
     end
 
-    def attr_diff(attr_name)
-      differ(attr_name).new(@a.send(attr_name), @b.send(attr_name)).diff
+    def attr_diff(a, b, attr_name)
+      differ(a, b, attr_name).new(a.send(attr_name), b.send(attr_name)).diff
     end
 
-    def differ(attr_name)
-      Caster.new(@a, @b).differ(attr_name)
-    end
-
-    def hash_diff(a, b)
-      HashDiff.new(a,b).diff
+    def differ(a, b, attr_name)
+      Caster.new(a, b).differ(attr_name)
     end
   end
 end
--- spec/object_diff/comparator_spec.rb
+++ spec/object_diff/comparator_spec.rb
@@ -6,9 +6,10 @@ module ObjectDiff
     it "gets diff by plain attrs key" do
       a = OpenStruct.new(:a => 1, :b => 2, :c => 3)
       b = OpenStruct.new(:a => 2, :b => 2, :c => 7)
-      comparator = Comparator.new(a, b)
 
-      expect(comparator.diff_by(plain_key: [:a, :b, :c])).to eq({
+      comparator = Comparator.new(plain_key: [:a, :b, :c])
+
+      expect(comparator.diff(a, b)).to eq({
         :a => [1, 2],
         :c => [3, 7]
       })
@@ -17,9 +18,9 @@ module ObjectDiff
     it "considers values of similar value but different kind as different" do
       a = OpenStruct.new(:a => 1, :b => "2")
       b = OpenStruct.new(:a => "1", :b => 2)
-      comparator = Comparator.new(a, b)
+      comparator = Comparator.new(plain_key: [:a, :b, :c])
 
-      expect(comparator.diff_by(plain_key: [:a, :b, :c])).to eq({
+      expect(comparator.diff(a, b)).to eq({
         :a => [1, "1"],
         :b => ["2", 2]
       })
@@ -28,9 +29,9 @@ module ObjectDiff
     it "gets diff by array" do
       a = OpenStruct.new(:a => 2, :b => [1, 2, 3])
       b = OpenStruct.new(:a => 2, :b => [2, 3, 4, 5])
-      comparator = Comparator.new(a, b)
+      comparator = Comparator.new(plain_key: [:a, :b])
 
-      expect(comparator.diff_by(plain_key: [:a, :b])).to eq({
+      expect(comparator.diff(a, b)).to eq({
         :b => {
           :added => [4, 5],
           :removed => [1]
@@ -49,9 +50,9 @@ module ObjectDiff
         :b1 => {:b2 => 20},
         :d1 => 12,
       })
-      comparator = Comparator.new(a, b)
+      comparator = Comparator.new(plain_key: [:a, :b])
 
-      expect(comparator.diff_by(plain_key: [:a, :b])).to eq({
+      expect(comparator.diff(a, b)).to eq({
         :b => {
           :a1 => [10, 5],
           :b1 => {
@@ -67,9 +68,9 @@ module ObjectDiff
       b1 = OpenStruct.new(:a1 => 11, :b2 => 12)
       b = OpenStruct.new(:a => b1)
 
-      comparator = Comparator.new(a,b)
+      comparator = Comparator.new(complex_key: {:a => [:a1, :b2]})
 
-      expect(comparator.diff_by(complex_key: {:a => [:a1, :b2]})).to eq({
+      expect(comparator.diff(a, b)).to eq({
         :a => {:a1 => [10, 11]}
       })
     end
